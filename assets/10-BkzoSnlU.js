89ccd9db77e8f13d50b795397ecff2ff5ae36d04
ssutsen <babe920604@gmail.com>
2024-11-06 00:54:39 +0800

    doc: vite_config

---
+++ assets/10-BkzoSnlU.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-COWQfF1Z.js";function r(){return i.jsx("div",{children:i.jsx("p",{children:"本專題特色在於採用更具體化指標, 以性格形容詞分辨適合科系,更準確判定, 以考慮校風,具體行為表現及個人情況進行分析並運用ai提升精確度, 更有互動性和趣味, 以遊戲作為媒介並整合其它升學資訊如ioh等以方便使用者進一步分析"})})}function e(){return i.jsx("div",{children:i.jsx("p",{children:"曾經作為高中生一員的我們也曾受到升學問題困擾, 所以是次專題動機就是希望借專題讓未來的高中生們不再為此而煩惱"})})}export{r as Component,e as Motive};
